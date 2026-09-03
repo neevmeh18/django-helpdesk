6347fcff54a9dbb3e7c3bf286ed43fdf62753a3a
Neev Mehta <neevmehta23.germany@gmail.com>
2026-09-03 14:06:08 +0200

    changed setting

--- src/helpdesk/settings.py
+++ src/helpdesk/settings.py
@@ -417,7 +417,7 @@ QUEUE_EMAIL_BOX_UPDATE_ONLY = getattr(settings, "QUEUE_EMAIL_BOX_UPDATE_ONLY", F
 
 # only allow users to access queues that they are members of?
 HELPDESK_ENABLE_PER_QUEUE_STAFF_PERMISSION = getattr(
-    settings, "HELPDESK_ENABLE_PER_QUEUE_STAFF_PERMISSION", False
+    settings, "HELPDESK_ENABLE_PER_QUEUE_STAFF_PERMISSION", True
 )
 
 # use https in the email links
